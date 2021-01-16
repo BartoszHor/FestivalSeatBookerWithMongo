61319e9862ace4c85c4c6f0a09d882e14971cc98
Bartosz Horoba <bartosz.horoba@gmail.com>
2021-01-16 21:17:31 +0100

    Add web sockets

---
+++ client/NewWaveFest/build/precache-manifest.f23096686fe3fccb50d03dbd82ee6290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7370ea982dd764665b7e5ca4815a5b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e027cf69b73cdcd89a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fc972f807f4dada3562b",
+    "url": "/static/css/main.0afa67f1.chunk.css"
+  },
+  {
+    "revision": "88e027cf69b73cdcd89a",
+    "url": "/static/js/2.5fa02ec6.chunk.js"
+  },
+  {
+    "revision": "fc972f807f4dada3562b",
+    "url": "/static/js/main.3a9bb61a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express()
 const socket = require('socket.io');
 
-const server = app.listen(8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
